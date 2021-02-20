03bce0a529edc13208055791e8ad8a8beec34c59
Bikram Bhattacharyya <73014174+stat1cprojectx@users.noreply.github.com>
2021-02-21 00:03:54 +0530

    add new encryption and minor updates

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require("mongoose");
+const md5 = require('md5');
 
-const encrypt = require("mongoose-encryption");
 const app = express();
-
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -25,11 +24,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String 
 });
-
-// schema encryption
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
-
 
 // model using userSchema
 const User = new mongoose.model("User",userSchema);
@@ -49,7 +43,7 @@ app.get("/register",(req,res)=>{
 app.post("/register",(req,res)=>{
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save((err)=>{
         if(err){
@@ -63,7 +57,7 @@ app.post("/register",(req,res)=>{
 
 app.post("/login",(req,res)=>{
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email:username},(err,foundUser)=>{
         if(err){
@@ -79,8 +73,7 @@ app.post("/login",(req,res)=>{
             }
         }
     });
-
-})
+});
 
 
 // app listener

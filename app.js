7fffedfc881e1b94bd455219e961bd7412f04213
Bikram Bhattacharyya <73014174+stat1cprojectx@users.noreply.github.com>
2021-02-21 14:01:39 +0530

    Add level 4 encryption

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require("mongoose");
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -41,23 +42,27 @@ app.get("/register",(req,res)=>{
 });
 
 app.post("/register",(req,res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save((err)=>{
-        if(err){
-            console.log(err);
-            res.send(err);
-        }else{
-            res.render("secrets");
-        }
-    });
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save((err)=>{
+            if(err){
+                console.log(err);
+                res.send(err);
+            }else{
+                res.render("secrets");
+            }
+        });
+    }); 
 });
 
 app.post("/login",(req,res)=>{
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email:username},(err,foundUser)=>{
         if(err){
@@ -65,9 +70,12 @@ app.post("/login",(req,res)=>{
             res.redirect("/login");
         }else{
             if (foundUser) {
-                if(foundUser.password==password){
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    // result == true
+                    if(result == true){
+                        res.render("secrets");
+                    }
+                });
             } else {
                 res.redirect("/login");
             }
